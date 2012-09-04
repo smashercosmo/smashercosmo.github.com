bbe4c097d8ea7cd62d2fa25ae72b27308ec02016
Vladislav Shkodin <smashercosmo@gmail.com>
2012-09-04 16:57:09 +0400

    test

--- OzSaleMobile/js/pages/shop.js
+++ OzSaleMobile/js/pages/shop.js
@@ -3,7 +3,7 @@ $(function(){
 		url: 'data/shop.json',
 		dataType: 'json',
 		success: function(data){
-			$('.sales').html(Handlebars.templates.shopSection(data.d));
+			$('.sales').html(JST.shopSection(data.d));
 		}
 	});
 });
