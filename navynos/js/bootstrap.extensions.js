8f049bc2152a783822b4a22aadb78e815406e1df
Vladislav Shkodin <smashercosmo@gmail.com>
2012-10-22 04:04:59 +0800

    layout update

---
+++ navynos/js/bootstrap.extensions.js
@@ -0,0 +1,11 @@
+/*
+$.fn.dropdownHover = function(){
+	return this.each(function(){
+		var $el = $(this);
+		$el
+			.on('click.dropdown.data-api', function(e){ e.stopImmediatePropagation() })
+			.on('mouseenter mouseleave', function(){ $el.dropdown('toggle') })
+			.next()
+			.on('mouseenter mouseleave', function(){ $el.dropdown('toggle') })
+	})
+};*/
