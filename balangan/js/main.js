5ee49fac668fbfa2d06eb34bccbc98efb44deae3
Vladislav Shkodin <smashercosmo@gmail.com>
2013-05-10 03:22:37 +0700

    buypoints page added + class names changed according to BEM methodology

--- balangan/js/main.js
+++ balangan/js/main.js
@@ -1,4 +1,47 @@
-$('.side-menu').on('click', '.side-menu-item-triangle', function(e){
+// toggle side menu sections
+$('.side-menu').on('click', '.side-menu__item__triangle', function(e){
 	$(this).closest('li').andSelf().toggleClass('opened');
 	e.preventDefault();
 });
+
+// Buy points slider
+(function(){
+
+	var POINTS_EXCHANGE_RATE = 43;
+
+	var moneyInput = $('.buy-points__control__field__input_money'),
+		pointsInput = $('.buy-points__control__field__input_points'),
+		leftArrow = $('.buy-points__control__slider__arrow_left'),
+		rightArrow = $('.buy-points__control__slider__arrow_right'),
+		slider = $('.buy-points__control__slider__range').slider({
+			orientation: 'horizontal',
+			range: 'min',
+			animate: false,
+			slide: bindInputs,
+			change: bindInputs
+		});
+
+	function bindInputs(event, ui){
+		moneyInput.val(ui.value);
+		pointsInput.val(ui.value * POINTS_EXCHANGE_RATE);
+	}
+
+	slider.slider('value', slider.data('value'));
+
+	moneyInput.on('change', function() {
+		slider.slider('value', moneyInput.val() );
+	});
+
+	pointsInput.on('change', function() {
+		slider.slider('value', pointsInput.val() / POINTS_EXCHANGE_RATE );
+	});
+
+	leftArrow.on('click', function(){
+		slider.slider('value', slider.slider('value') - 1 );
+	});
+
+	rightArrow.on('click', function(){
+		slider.slider('value', slider.slider('value') + 1 );
+	});
+
+}());
