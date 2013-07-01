72f7187d1bb2403cea7145467c974708c09bdcaa
Vladislav Shkodin <smashercosmo@gmail.com>
2013-07-01 18:29:03 +0400

    sticky demo

--- sticky/main.js
+++ sticky/main.js
@@ -1,14 +1,33 @@
 $(function(){
 
-    $mainNav = $('.main-nav');
+    var $mainNav = $('.main-nav'),
+        sticked = false;
 
     $mainNav.on('click', 'a', function(e){
         $(e.target).next('.popup').toggleClass('popup_opened');
+        e.preventDefault();
     });
 
     $clonedNav = $mainNav.clone(true);
-    $clonedNav.css({ position: 'fixed', top: 0, left: 0 });
+    $clonedNav.css({ position: 'fixed', top: 0, left: 0, visibility: 'hidden' });
     $clonedNav.insertAfter($mainNav);
 
+    function scroller(){
+        var scrollTop = $(window).scrollTop();
+        if(scrollTop >= 100 && !sticked){
+            sticked = true;
+            $mainNav.css({ visibility: 'hidden' });
+            $clonedNav.css({ visibility: 'visible' });
+
+        } else if (scrollTop < 100 && sticked) {
+            sticked = false;
+            $mainNav.css({ visibility: 'visible' });
+            $clonedNav.css({ visibility: 'hidden' });
+        }
+    }
+
+    scroller();
+
+    window.addEventListener('scroll', scroller, false);
 
 });
